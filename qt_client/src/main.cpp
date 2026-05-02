445e8a58761624a47a72a2cdd418a589e2cd2772
wuming <wuming@localhost>
2026-05-02 19:00:34 +0800

    改用xcb，原先的兼容性问题有可能导致最大化失败

--- qt_client/src/main.cpp
+++ qt_client/src/main.cpp
@@ -1,7 +1,11 @@
 #include <QApplication>
+#include <QByteArray>
 #include "MainWindow.h"
 
 int main(int argc, char* argv[]) {
+    if (qEnvironmentVariableIsEmpty("QT_QPA_PLATFORM")) {
+        qputenv("QT_QPA_PLATFORM", QByteArray("xcb"));
+    }
     QApplication app(argc, argv);
     MainWindow window;
     window.show();
